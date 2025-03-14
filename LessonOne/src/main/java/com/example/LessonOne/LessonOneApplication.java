8db18f17845def15f3e43c3d2730664a56fd2f39
DILSHAN PATHIRAGE <danidutharuka12345@gmail.com>
2025-03-14 18:46:29 +0530

    Create demo

---
+++ LessonOne/src/main/java/com/example/LessonOne/LessonOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.LessonOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LessonOneApplication.class, args);
+	}
+
+}
---
+++ LessonOne/src/test/java/com/example/LessonOne/LessonOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LessonOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
